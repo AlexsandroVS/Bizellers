76aacfbf8c77db26fb03df368c40e9d86c1e8a7a
AlexsandroVS <alexsandro.valeriano1@gmail.com>
2025-11-17 17:01:56 -0500

    pruebas de rutas

--- api/export/index.ts
+++ api/export/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import prisma from '@/lib/prisma.js'; // Import shared prisma instance
+import  prisma  from '../../src/lib/prisma';
 import * as XLSX from 'xlsx';
 
 // Helper para verificar la autenticación
